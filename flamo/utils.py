23b326812d918660a657f11975c54de4b1de1671
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2024-09-24 16:35:04 +0300

    Add to_complex method

--- flamo/utils.py
+++ flamo/utils.py
@@ -3,3 +3,15 @@ import torch
 def get_device():
     r""" Output 'cuda' if gpu is available, 'cpu' otherwise """
     return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def to_complex(x):
+    r"""
+    Converts a real tensor to a complex tensor.
+
+        **Args**:
+            x (torch.Tensor): The input tensor.
+
+        **Returns**:
+            torch.Tensor: The complex tensor with the same shape as the input tensor.
+    """
+    return torch.complex(x, torch.zeros_like(x))
