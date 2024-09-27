998f42ed2f27a9d9988594c3ad4243a8f0fab459
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2024-09-27 15:40:25 +0300

    save audio and parameters

--- flamo/examples/example_colorless_fdn.py
+++ flamo/examples/example_colorless_fdn.py
@@ -2,15 +2,23 @@ import torch
 import argparse
 import os
 import time
+import scipy
 from collections import OrderedDict
 from flamo.optimize.dataset import DatasetColorless, load_dataset
 from flamo.optimize.trainer import Trainer
 from flamo.processor import dsp, system
 from flamo.optimize.loss import mse_loss, sparsity_loss
-
+from flamo.utils import save_audio
 torch.manual_seed(130709)
 
 def example_fdn(args):
+    """
+    Example function that demonstrates the construction and training of a Feedback Delay Network (FDN) model.
+    Args:
+        args: A dictionary or object containing the necessary arguments for the function.
+    Returns:
+        None
+    """
 
     # FDN parameters
     N = 6  # number of delays
@@ -19,14 +27,14 @@ def example_fdn(args):
 
     ## ---------------- CONSTRUCT FDN ---------------- ##
 
-    # input and output gains
+    # Input and output gains
     input_gain = dsp.Gain(
         size=(N, 1), nfft=args.nfft, requires_grad=True, alias_decay_db=alias_decay_db
     )
     output_gain = dsp.Gain(
         size=(1, N), nfft=args.nfft, requires_grad=True, alias_decay_db=alias_decay_db
     )
-    # feedback loop with delays
+    # Feedback loop with delays
     delays = dsp.parallelDelay(
         size=(N,),
         max_len=delay_lengths.max(),
@@ -36,7 +44,7 @@ def example_fdn(args):
         alias_decay_db=alias_decay_db,
     )
     delays.assign_value(delays.sample2s(delay_lengths))
-    # feedback path with orthogonal matrix
+    # Feedback path with orthogonal matrix
     feedback = dsp.Matrix(
         size=(N, N),
         nfft=args.nfft,
@@ -44,10 +52,10 @@ def example_fdn(args):
         requires_grad=True,
         alias_decay_db=alias_decay_db,
     )
-    # recursion
+    # Recursion
     feedback_loop = system.Recursion(fF=delays, fB=feedback)
 
-    # full FDN
+    # Full FDN
     FDN = system.Series(OrderedDict({
         'input_gain': input_gain,
         'feedback_loop': feedback_loop,
@@ -58,6 +66,12 @@ def example_fdn(args):
     input_layer = dsp.FFT(args.nfft)
     output_layer = dsp.Transform(transform=lambda x : torch.abs(x))
     model = system.Shell(core=FDN, input_layer=input_layer, output_layer=output_layer)
+
+    # Get initial impulse response
+    with torch.no_grad():
+        ir_init =  model.get_time_response(interior=False, fs=args.samplerate).squeeze() 
+        save_audio(os.path.join(args.train_dir, "ir_init.wav"), ir_init/torch.max(torch.abs(ir_init)), fs=args.samplerate)
+        save_fdn_params(model, filename='parameters_init')
 
     ## ---------------- OPTIMIZATION SET UP ---------------- ##
 
@@ -79,9 +93,37 @@ def example_fdn(args):
     # Train the model
     trainer.train(train_loader, valid_loader)
 
+    # Get optimized impulse response
     with torch.no_grad():
-        ir_optim =  model.get_time_response(interior=False, fs=args.samplerate).squeeze().sum(-1) # TODO implement exp_decay unwrapper
+        ir_optim =  model.get_time_response(interior=False, fs=args.samplerate).squeeze()
+        save_audio(os.path.join(args.train_dir, "ir_optim.wav"), ir_optim/torch.max(torch.abs(ir_optim)), fs=args.samplerate)
+        save_fdn_params(model, filename='parameters_optim')
 
+def save_fdn_params(net, filename='parameters'):
+    r"""
+    Retrieves the parameters of a feedback delay network (FDN) from a given network and saves them in .mat format.
+
+    **Parameters**:
+        net (Shell): The Shell class containing the FDN.
+        filename (str): The name of the file to save the parameters without file extension.
+    **Returns**:
+        dict: A dictionary containing the FDN parameters.
+            - 'A' (ndarray): The feedback loop parameter A.
+            - 'B' (ndarray): The input gain parameter B.
+            - 'C' (ndarray): The output gain parameter C.
+            - 'm' (ndarray): The feedforward parameter m.
+    """
+
+    core = net.get_core()
+    param = {}
+    param['A'] = core.feedback_loop.feedback.param.squeeze().detach().cpu().numpy()
+    param['B'] = core.input_gain.param.squeeze().detach().cpu().numpy()
+    param['C'] = core.output_gain.param.squeeze().detach().cpu().numpy()
+    param['m'] = core.feedback_loop.feedforward.s2sample(core.feedback_loop.feedforward.map(core.feedback_loop.feedforward.param)).squeeze().detach().cpu().numpy()
+
+    scipy.io.savemat(os.path.join(args.train_dir, filename + '.mat'), param)
+
+    return param
 
 if __name__ == "__main__":
 
