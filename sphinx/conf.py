75bf78115646d33400f3168bffe9f7c8d77b4979
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2025-01-24 11:32:31 +0200

    Fix sphinx

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -37,6 +37,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'alabaster'
+# html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
