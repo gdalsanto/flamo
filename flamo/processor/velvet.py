71b1cd127310429acadb2ea6b065878059fe8bd6
cucuwritescode <anicsounds@gmail.com>
2025-07-31 19:26:08 -0700

    velvet noise sequence class
    dropped the velvet noise matrix class with the purpose of modularity and splitting it in different classes. now it's velvet noise sequence which I plot and successfully generates a velvet noise sequence. spaced randomly in the time domain, values +1 or -1 and everything else is zero

--- flamo/processor/velvet.py
+++ flamo/processor/velvet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import math
+import matplotlib.pyplot as plt
 
 """basicslly velvet noise matrix is a pytorch module that builds a 1D time-domain signal/tensor (multidim array) made of 
  -randomly placed impulses 
@@ -13,7 +14,7 @@ Velvet-Noise Sequences" by Vesa Välimäki and Karolina Prawda.
 
 """
 
-class VelvetNoiseMatrix(nn.Module): #make it a pytorch module
+class VelvetNoiseSequence(nn.Module): #make it a pytorch module
         def __init__(self, length: int, density: float, sample_rate: int = 48000, decay_db: float = 60.0):
             super().__init__()
             self.length = length #how many samples long the output sequence is
@@ -23,17 +24,15 @@ class VelvetNoiseMatrix(nn.Module): #make it a pytorch module
             self.register_buffer("sequence", self._generate()) #store generated signal as a buffer in self.sequence
             #is this last line correct? buffers as they are named tensors that do not update gradients during training 
 
-
         #next write a generate method that generates the actual velvet noise seq 
         def _generate(self):
             Td = self.sample_rate / self.density  #td is grid size!!!!
             num_impulses = self.length / Td #number of impulses in the sequence, 
             floor_impulses=math.floor(num_impulses) #round down to the nearest integer
 
-            
             #first generate fixed grid positions where impulses will be placed
             grid_positions = torch.arange(floor_impulses) * Td
-            
+
             #next generate random ppositions for the impulses with jitter (como en el algoritmo q me mostro gloria)
             #generate random jitter factors between 0 and 1 for each impulse
             jitter_factors= torch.rand(floor_impulses) #how much jitter to apply to each impulse?
@@ -48,9 +47,49 @@ class VelvetNoiseMatrix(nn.Module): #make it a pytorch module
             sign = 2 * torch.randint(0, 2, (floor_impulses,)) - 1
             print(sign)
 
+            #now construct the actual sparse signal (1D tensor) with impulses
+            sequence = torch.zeros(self.length)
+            sequence[impulse_indices] = sign.float()
 
-            return torch.zeros(self.length)
+            return sequence
+        
 if __name__ == "__main__":
-    vnm = VelvetNoiseMatrix(length=48000, density=100)
+    vnm = VelvetNoiseSequence(length=48000, density=100)
     print("generated velvet noise sequence:")
     print(vnm.sequence)    
+    #plot 
+    plt.plot(vnm.sequence[:2000].numpy())
+    plt.figure(figsize=(12, 4))
+    plt.plot(vnm.sequence.numpy())
+    plt.title("Velvet Noise Sequence")
+    plt.xlabel("Sample Index")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+"""
+what's done so far:
+ VelvetNoiseSequence is done, a 1D generator
+"""
+
+#TODO!!!!!
+"""next steps wld be 
+generate multiple velvet noise sequences, one for each of the n^2 entries in the matrix, or for each filter path (more efficient???)
+then filter each sequence (IIR or FIR??) to shape the spectral prof of each particular sequence 
+then stack em into a 3d matrix (tensor). each filtered signal bwcomes a time domain "impulse response" for a corresponding matrix entry
+tensor shld look like: NxNxT
+
+the 3d matrix/tensor could be used in flamo 
+
+
+in short:::::
+now VelvetNoiseBank, Create NxN of them, shape (N, N, T)
+then filtering step to make each 1D sequence smoother
+last return a torch.Tensor of shape (N, N, T)
+
+
+"""
